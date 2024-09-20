d839fd9051e1789b772a8f65a3ca2c93f4af6be1
Ido madaRR <idox2x@gmail.com>
2024-09-20 21:30:41 +0300

    ts

---
+++ dist/models/RoadRecord.js
@@ -0,0 +1,61 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RoadRecord = void 0;
+const mongoose_1 = require("mongoose");
+const haversineFormula_1 = require("../utils/haversineFormula");
+const roadRecordSchema = new mongoose_1.Schema({
+    deviceId: {
+        type: String,
+        require: true,
+    },
+    distance: {
+        type: Number,
+        require: true,
+        default: 0,
+    },
+    averageSpeed: {
+        type: Number,
+        require: true,
+    },
+    startTime: {
+        type: Date,
+        require: true,
+    },
+    endTime: {
+        type: Date,
+        default: new Date(),
+    },
+    waypoints: {
+        type: [
+            {
+                _id: false,
+                longitude: {
+                    type: Number,
+                    required: true,
+                },
+                latitude: {
+                    type: Number,
+                    required: true,
+                },
+            },
+        ],
+        required: true,
+    },
+    image: {
+        type: String,
+        default: null,
+    },
+}, {
+    versionKey: false,
+});
+roadRecordSchema.statics.build = (record, deviceId) => {
+    const { distance, averageSpeed, startTime, waypoints } = (0, haversineFormula_1.analyzeRoadRecord)(record);
+    return new exports.RoadRecord({
+        deviceId,
+        distance,
+        averageSpeed,
+        startTime,
+        waypoints,
+    });
+};
+exports.RoadRecord = (0, mongoose_1.model)('Record', roadRecordSchema);
